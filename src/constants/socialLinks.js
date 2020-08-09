0feddf025fda71a3d5efe180a47957d9a8994215
LinuxLite <sk7_forever@yahoo.com>
2020-08-09 00:14:13 -0500

    Hero listo

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -2,36 +2,46 @@ import React from "react"
 import {
   FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
+  FaGithubSquare,
   FaTwitterSquare,
+  FaWordpress, FaBlogger, FaYoutube
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    url: "https://www.twitter.com/karlosrocker",
   },
   {
     id: 2,
-    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: "https://github.com/skcode7/",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/carlos-encalada-4830b383/",
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
+    url: "facebook.com/karlos.encalada.3/",
   },
   {
     id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaWordpress className="social-icon"></FaWordpress>,
+    url: "https://karlosencalada.wordpress.com/",
+  },
+  {
+    id: 6,
+    icon: <FaBlogger className="social-icon"></FaBlogger>,
+    url: "https://sk-forever.blogspot.com/",
+  },
+  {
+    id: 7,
+    icon: <FaYoutube className="social-icon"></FaYoutube>,
+    url: "https://www.youtube.com/c/CarlosEncalada7/",
   },
 ]
 const links = data.map(link => {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { FaTelegramPlane, FaWhatsapp } from "react-icons/fa"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <div>
+      <h2>contact page</h2>
+      <a href="https://t.me/karlosrocker" className="social-link">
+        <FaTelegramPlane className="social-icon"></FaTelegramPlane>
+      </a>
+      <a href="https://wa.me/59399594717" className="social-link">
+        <FaWhatsapp className="social-icon"></FaWhatsapp>
+      </a>
+    </div>
+  )
+
 }
 
 export default contact
