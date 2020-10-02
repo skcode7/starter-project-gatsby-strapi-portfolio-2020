e3ac790deab2ada2f32323454856dd058c94bda6
LinuxLite <sk7_forever@yahoo.com>
2020-10-01 22:53:22 -0500

    Ajustes en Servicios

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,21 +5,21 @@ export default [
     id: 1,
     icon: <FaCopyright className="service-icon" />,
     title: "Consultoria Informatica",
-    text: `COIN-JIH es una empresa especializada en ofrecer servicios y soluciones en el ámbito de las tecnologías de la información y el desarrollo de software.`,
+    text: `COIN-JIH es una empresa especializada en ofrecer servicios y soluciones en el ámbito de las tecnologías de la información y el desarrollo de software. ERPs, Facturacion Electronica, Gestion Documental, entre otros conforman su portafolio de soluciones.`,
     url: "http://coin-jih.com/",
   },
   {
     id: 3,
     icon: <FaBriefcase className="service-icon" />,
-    title: "Soluciones Tecnologicas",
-    text: `EncDia es un emprendimiento que busca ofrecer soluciones multiples: desarrollo de software, diseno grafico, asesoria digital, enfocada principalmente a peque;os negocios o emprendimientos`,
+    title: "Desarrollo a Medida",
+    text: `EncDia es un emprendimiento que busca ofrecer soluciones tecnologicas: desarrollo de software, diseno grafico, asesoria digital, enfocada principalmente a peque;os negocios o emprendimientos`,
     url: "https://desarrolloamedida.net/"
   },
   {
     id: 2,
     icon: <FaCode className="service-icon" />,
     title: "Kode by SK",
-    text: `Proyectos personales, desarrollo a medida, pruebas de concepto, asesoria, etc.`,
+    text: `Proyectos personales, pruebas de concepto, proyetos de divulgacion y para compartir conocimiento.`,
     url: "#",
   },
 ]
